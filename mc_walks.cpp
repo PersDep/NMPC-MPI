4739f1ad5041ec676852886bea86bc5ae46d5fcc
Ilya Kosarev <ilyanapar@yandex.ru>
2019-01-12 18:44:33 +0300

    Finished mc_walks

--- mc_walks.cpp
+++ mc_walks.cpp
@@ -3,6 +3,9 @@
 #include <cstdlib>
 #include <cmath>
 #include <ctime>
+#include <string>
+
+#include "mpi.h"
 
 using namespace std;
 
@@ -26,33 +29,56 @@ int do_walk(int a, int b, int x, double p, double& t)
     return x;
 }
 
-void run_mc(int a, int b, int x, double p, int N)
-{
+void run_mc(int a, int b, int x, double p, int N, int proc_amount = 1, int proc_rank = 0) {
     // srand(time(0));
     double t = 0.0;
     double w = 0.0;
 
-    for( int i=0; i<N; i++ )
-    {
+    int overhead = N % proc_amount;
+    int block_size = N / proc_amount;
+    if (proc_rank < overhead)
+        block_size++;
+    int start = block_size * proc_rank;
+    if (proc_rank >= overhead)
+        start += overhead;
+    for (int i = start; i < start + block_size; i++) {
         int out = do_walk(a, b, x, p, t);
-        if( out == b )
+        if (out == b)
             w += 1;
     }
 
-    ofstream f("output.dat");
-    f << w/N << " " << t/N << endl;
-    f.close();
+    double t_common, w_common;
+    MPI_Reduce(&t, &t_common, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&w, &w_common, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (!proc_rank) {
+        ofstream f("output_" + to_string((long long)(N)) + '_' + to_string((long long)(proc_amount)) + ".txt");
+        f << w_common / N << " " << t_common / N << endl;
+        f.close();
+    }
 }
 
 int main(int argc, char** argv)
 {
+    MPI_Init(&argc, &argv);
+    int proc_amount, proc_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &proc_amount);
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
+
     int a = atoi(argv[1]);
     int b = atoi(argv[2]);
     int x = atoi(argv[3]);
     double p = atof(argv[4]);
     int N = atoi(argv[5]);
 
-    run_mc(a, b, x, p, N);
+    double start = MPI_Wtime();
+    run_mc(a, b, x, p, N, proc_amount, proc_rank);
+    if (!proc_rank) {
+        ofstream f("stat_" + to_string((long long)(N)) + '_' + to_string((long long)(proc_amount)) + ".txt");
+        f << a << ' ' << b << ' ' << x << ' ' << p << ' ' << N << endl << MPI_Wtime() - start << ' ' << proc_amount << endl;
+        f.close();
+    }
+    MPI_Finalize();
 
     return 0;
 }
